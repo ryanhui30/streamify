250df6e5b12f42efdfed9e38069a004ac312d7bf
Ryan Hui <ryanhui30@gmail.com>
2025-05-15 23:59:02 -0700

    Add backend auth setup

---
+++ backend/src/controllers/auth.controller.js
@@ -0,0 +1,11 @@
+export async function signup(req, res) {
+    res.send("Signup Route");
+}
+
+export async function signup(req, res) {
+    res.send("Login Route");
+}
+
+export function signup(req, res) {
+    res.send("Logout Route");
+}
