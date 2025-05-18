0a040edf22d3a181b0c2a8f577535913ce256e50
Ryan Hui <ryanhui30@gmail.com>
2025-05-17 18:56:27 -0700

    Add onboard endpoint

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,7 +1,9 @@
 import express from "express";
 import "dotenv/config";
+import cookieParser from "cookie-parser";
 
 import authRoutes from "./routes/auth.route.js";
+import { connectDB } from "./lib/db.js";
 
 const app = express();
 const PORT = process.env.PORT;
