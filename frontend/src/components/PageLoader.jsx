80a5d9bc49c992233df9f551933e8b562dc5c5ed
Ryan Hui <ryanhui30@gmail.com>
2025-05-19 16:11:01 -0700

    Add onboarding page

--- frontend/src/components/PageLoader.jsx
+++ frontend/src/components/PageLoader.jsx
@@ -1,9 +1,12 @@
-import React from 'react'
+import { LoaderIcon } from "lucide-react";
+import { useThemeStore } from "../store/useThemeStore";
 
 const PageLoader = () => {
+  const { theme } = useThemeStore();
   return (
-    <div>PageLoader</div>
-  )
-}
-
-export default PageLoader
+    <div className="min-h-screen flex items-center justify-center" data-theme={theme}>
+      <LoaderIcon className="animate-spin size-10 text-primary" />
+    </div>
+  );
+};
+export default PageLoader;
