5642082886dd3b751683eef74f19dcf08c4d4370
Ryan Hui <ryanhui30@gmail.com>
2025-05-21 01:39:05 -0700

    Add chat page

--- frontend/src/components/ChatLoader.jsx
+++ frontend/src/components/ChatLoader.jsx
@@ -1,9 +1,12 @@
-import React from 'react'
+import { LoaderIcon } from "lucide-react";
 
-const ChatLoader = () => {
+function ChatLoader() {
   return (
-    <div>ChatLoader</div>
-  )
+    <div className="h-screen flex flex-col items-center justify-center p-4">
+      <LoaderIcon className="animate-spin size-10 text-primary" />
+      <p className="mt-4 text-center text-lg font-mono">Connecting to chat...</p>
+    </div>
+  );
 }
 
-export default ChatLoader
+export default ChatLoader;
